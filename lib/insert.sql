c4a8ea8073fdce315db3659d932f969b132530ab
Marvin Reyes <reyemarv@gmail.com>
2020-08-02 03:48:22 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,1 @@
-INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", )
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 3, "Male", "Black", "Low", 1);

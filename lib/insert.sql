e51d5fcf56725817c2c3095128189d5538e22a0c
Marvin Reyes <reyemarv@gmail.com>
2020-08-02 03:53:15 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,8 @@
 INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 3, "Male", "Black", "Low", 1);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Rowdy", 1, "Male", "Brown", "Med", 1);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Tabitha", 3, "Female", "Black", "Med", 1);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 5, "Male", "Brown", "High", 1);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Melissa", 4, "Female", "White", "Low", 1);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Grinch", 2, "Male", "White", "High", 1);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Wendy", 1, "Female", "Brown", "Low", 1);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES (NULL, 4, "Male", "Black", "High", 1);
